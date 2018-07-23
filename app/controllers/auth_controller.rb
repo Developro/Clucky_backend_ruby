de05e71d46f6637d7baf1b71b57ed2f1e0f47f24
Dmitry Fursenko <dfursenko@gmail.com>
2018-07-24 01:12:05 +0700

    add Auth controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,18 @@
+class AuthController < ApplicationController
+  def login
+    if params
+
+    else
+      # send 400
+    end
+  end
+
+  def logout
+  end
+
+  def refresh
+  end
+
+  def register
+  end
+end
